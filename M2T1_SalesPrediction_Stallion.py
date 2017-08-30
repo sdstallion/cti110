08f2590e0d8f65bbae0c60cb3dbd1706a364b800
sdstallion <31459097+sdstallion@users.noreply.github.com>
2017-08-29 21:03:41 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_Stallion.py
@@ -0,0 +1,15 @@
+
+# CTI-110
+# ﻿M2T1 - Sales Prediction﻿﻿﻿﻿﻿
+# Samuel Stallion
+# 29 AUG 2017﻿﻿﻿﻿﻿
+#
+
+# Get the projected total sales.
+total_sales = float(input( 'Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print ('The profit is $',format(profit,',.2f'))
